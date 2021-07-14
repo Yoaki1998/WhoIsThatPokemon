328cad0c4a7e142eaeaaf3ed2c2f9f173a1d9fb1
Yoaki Niscoise <yoakiniscoise@gmail.com>
2021-07-14 11:50:20 +0200

    Gestion des données de la pokeApi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  state = {
+    pokemons: []
+  }
+
+  pokeapi = () => {
+    const randomizeAll = () => Math.floor(Math.random() * (150 - 0 + 1)) + 0
+    const randomize = () => Math.floor(Math.random() * (2 - 0 + 1)) + 0    
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json())
+      .then((data) => { 
+        const pokeall = data.results
+        const pokeChoice = []
+        for (let i = 0 ; i < 3; i++) {
+          pokeChoice.push(pokeall[randomizeAll()])
+        }
+        const winner = pokeChoice[randomize()]
+        console.log(pokeChoice)
+        console.log(winner)
+      })
+  }
+
+
+  componentDidMount () {
+    this.pokeapi()
+  }
+
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
