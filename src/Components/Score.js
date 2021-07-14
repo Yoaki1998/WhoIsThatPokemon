49cccb603049540b3e1582ebd5256940cf184433
Yoaki Niscoise <yoakiniscoise@gmail.com>
2021-07-14 23:19:59 +0200

    Fix responsive

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Score = ({score}) => {
     return (
-        <div>
+        <div className="letrou">
             <h3> SCORE : {score} </h3>
         </div>
     )
